a11899cc96e246783d104417c9c1fa72741f198d
“lukemccrae” <lukemccrae@gmail.com>
2019-04-02 11:21:04 -0700

    post route connecting

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/api/account/signup', function(req, res, next) {
+  res.send(req.body)
+});
+
 module.exports = router;
