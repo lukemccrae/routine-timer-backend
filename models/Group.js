086b4bde324c31b7eaac742df5bd078b7f212ecb
“lukemccrae” <lukemccrae@gmail.com>
2019-05-15 10:59:59 -0700

    changes

--- models/Group.js
+++ models/Group.js
@@ -4,13 +4,18 @@ const bcrypt = require('bcrypt')
 const GroupSchema = new mongoose.Schema({
   name: {
     type: String,
-    default: 'New Group'
+    default: 'New Group',
+    required: true
   },
   timers: {
     type: Array,
     default: []
   },
   user: {
+    type: String,
+    required: true
+  },
+  hash: {
     type: String,
     required: true
   }
