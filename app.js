56fd04f5c52b6cc03773f6564dc376d42090c8b0
“lukemccrae” <lukemccrae@gmail.com>
2019-05-27 13:22:22 -0700

    set mongolab_uri

--- app.js
+++ app.js
@@ -19,29 +19,29 @@ const MONGODB_URI = process.env.MONGOLAB_URI || process.env.LOCAL_HOST;
 // var mongodb = process.env.API_HOST;
 console.log(MONGODB_URI, 'mongodb log');
 
-mongoose.connect(MONGODB_URI, {useNewUrlParser: true});
+;
 
-// const options = {
-//     autoIndex: false, // Don't build indexes
-//     reconnectTries: 30, // Retry up to 30 times
-//     reconnectInterval: 500, // Reconnect every 500ms
-//     poolSize: 10, // Maintain up to 10 socket connections
-//     // If not connected, return errors immediately rather than waiting for reconnect
-//     bufferMaxEntries: 0,
-//     useNewUrlParser: true
-//   }
+const options = {
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+    useNewUrlParser: true
+  }
 
-// const connectWithRetry = () => {
-//   console.log('MongoDB connection with retry')
-//   mongoose.connect(mongodb, options).then(()=>{
-//     console.log('MongoDB is connected')
-//   }).catch(err=>{
-//     console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
-//     setTimeout(connectWithRetry, 5000)
-//   })
-// }
+const connectWithRetry = () => {
+  console.log('MongoDB connection with retry')
+  mongoose.connect(MONGODB_URI, {useNewUrlParser: true}).then(()=>{
+    console.log('MongoDB is connected')
+  }).catch(err=>{
+    console.log('MongoDB connection unsuccessful, retry after 5 seconds.')
+    setTimeout(connectWithRetry, 5000)
+  })
+}
 
-// connectWithRetry()
+connectWithRetry()
 
 //Get the default connection
 var db = mongoose.connection;
