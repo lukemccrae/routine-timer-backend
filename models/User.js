224bd10702cba4a9bdb988a9790c6aebfc393a85
“lukemccrae” <lukemccrae@gmail.com>
2021-05-05 09:19:09 -0700

    fix cors

--- models/User.js
+++ models/User.js
@@ -28,7 +28,6 @@ const UserSchema = new mongoose.Schema({
   }
 });
 
-
 UserSchema.methods.generateHash = function(password, userInfo) {
   return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
 }
