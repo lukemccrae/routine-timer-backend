246f39e1ed7ef6f7aea9c9b888bc626029918722
“lukemccrae” <lukemccrae@gmail.com>
2021-05-05 12:07:57 -0700

    fix cors

--- app.js
+++ app.js
@@ -66,6 +66,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
   }
 
+  
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
