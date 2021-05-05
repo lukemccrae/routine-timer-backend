77d7c64fb056c0e698b3c9cf66c728e98783dfdf
“lukemccrae” <lukemccrae@gmail.com>
2021-05-05 09:58:51 -0700

    fix cors

--- app.js
+++ app.js
@@ -56,6 +56,7 @@ app.use(cors());
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
