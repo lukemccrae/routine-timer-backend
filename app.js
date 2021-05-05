e3dfe3084f737965ad17d0cd63cf809dca0289b1
“lukemccrae” <lukemccrae@gmail.com>
2021-05-05 10:34:10 -0700

    fix cors

--- app.js
+++ app.js
@@ -58,7 +58,9 @@ app.use(cors());
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', 'https://group-timer.firebaseapp.com/');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
