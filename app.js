95bdcb4f9a1dc4077a8d2c7ef1b0921d4cf91799
“lukemccrae” <lukemccrae@gmail.com>
2021-05-05 10:32:12 -0700

    fix cors

--- app.js
+++ app.js
@@ -53,6 +53,27 @@ var app = express();
 
 app.use(cors());
 
+//Hopefully this will help with CORS... holy shit i am so done with this problem
+// Add headers
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 //Bind connection to error event (to get notification of connection errors)
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
